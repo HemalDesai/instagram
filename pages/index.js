17ef3d0a48fa78a1c3dfd2261bc5c0c025bf98d8
HemalDesai <hemald332@gmail.com>
2023-02-23 16:11:41 +0530

    converted tsx to js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import type { NextPage } from 'next'
+// import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Feed from '../components/Feed'
 import Modal from '../components/Modal'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className="">
       <Head>
